e42529bb16f931654de2ea2c13610a58ce8a67e3
maha-ahmad1 <maha.ahmad.k@hotmail.com>
2025-08-23 11:09:04 +0300

    fix the images issue

--- components/Client/ClientWrapper.jsx
+++ components/Client/ClientWrapper.jsx
@@ -1,5 +1,7 @@
 "use client"
 
+import 'lazysizes';
+
 import { AuthProvider, useAuth } from "@/utils/AuthContext";
 import { LangProvider } from "@/utils/langContext";
 import LogicProvider from "./LogicProvider";
